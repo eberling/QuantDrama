5926132d73b5b048c543e3e0c4e187cd988d84e3
eberling <c.eberling93@gmail.com>
2020-02-21 16:51:06 +0100

    improved regex, added episode class

--- public/index.js
+++ public/index.js
@@ -4,6 +4,25 @@ const cheerio = require("cheerio");
 var admin = require("firebase-admin");
 var serviceAccount = require("../firebase-admin-token.json");
 
+class Episode {
+  epTitle;
+  epChars;
+  epScenes;
+  constructor(epTitle, epChars, epScenes) {
+    this.epTitle = epTitle;
+    this.epChars = epChars;
+    this.epScenes = epScenes;
+  }
+}
+class EpScene {
+  scTitle;
+  scChars;
+  constructor(scTitle, scChars) {
+    this.scTitle = scTitle;
+    this.scChars = scChars;
+  }
+}
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://quantdrama.firebaseio.com"
@@ -17,10 +36,8 @@ async function main() {
   //   })
   // );
 
-  const transcripts = await getTranscript(
-    "http://www.chakoteya.net/DS9/432.htm"
-  );
-
+  const episode = await getEpisode("http://www.chakoteya.net/DS9/447.htm");
+  console.log(episode.epScenes);
   //console.log(transcripts.text());
   //
 
@@ -35,71 +52,49 @@ async function main() {
   // );
 }
 
-const getTranscript = async url => {
+const getEpisode = async url => {
+  const episode = new Episode();
   try {
     const html = await axios.get(url);
     const $ = cheerio.load(html.data);
-    const transcriptNodes = $("div p font");
-    console.log("important");
-    const nonUniqueChars = transcriptNodes.text().match(/[A-Z][?'A-Z]+(?=:)/g);
+    episode.epTitle = $("body>p>font>b").text();
+
+    const transcript = $("div font");
+    const nonUniqueChars = getChars(transcript.text());
     const dedupe = new Set(nonUniqueChars);
-    const characters = [...dedupe];
-    console.warn("# of Characters: ", characters.length);
-    console.log(characters);
-    console.log("\n");
+    episode.epChars = [...dedupe];
 
-    const episode = {
-      epTitle: $("body>p>font>b").text(),
-      epCharacters: characters,
-      epScenes: []
-    };
+    const dialogAndScenes = transcript.text().split(/(\[(?:(?!\[).)+\])(?!:)/g);
 
-    const charsAndScenes = transcriptNodes.map((i, node) => {
-      const $ = cheerio.load(node);
-      // some episodes start with a "last time..." intro
-      if (i === 0 && !$("b").text()) {
-        console.log("strange", $("b").text(), i);
-        console.log("Last time... discarded", $("*").text());
-      } else if ($("b").text()) {
-        return getScene(i, node);
+    dialogAndScenes.forEach((element, i) => {
+      const isScene = element.match(/^(\[.*\])$/g);
+      if (isScene) {
+        if (!episode.epScenes) {
+          episode.epScenes = new Array();
+        }
+        episode.epScenes.push(new EpScene(element));
+        //console.log("new Scene:", episode.epScenes.slice(-1)[0].scTitle);
+      } else if (episode.epScenes) {
+        episode.epScenes.slice(-1)[0].scChars = getChars(element);
+        //console.log("Scene:", episode.epScenes.slice(-1)[0]);
       } else {
-        return getSceneChars(node);
+        console.log(element, "discarded");
       }
     });
-    console.log(charsAndScenes.get());
+    return episode;
   } catch (e) {
-    console.log("[error getting Transcripts] ", e);
+    console.log("[error building episode object:] ", e);
   }
 };
 
-const getScene = (i, node) => {
-  const $ = cheerio.load(node);
-  const sceneTitle = $("b").text();
-  if (sceneTitle) {
-    console.log("New Scene!: ", sceneTitle);
-    return sceneTitle;
-    // epTranscript.epScenes.push({
-    //   sceneTitle: sceneTitle,
-    //   sceneChars: []
-    // });
-  } else {
-    console.warn("Something went wrong - no Scene Name Found");
-  }
-};
-
-const getSceneChars = node => {
-  // let epScene = {
-  //   sceneTitle: sceneTitle,
-  //   sceneChars: []
-  // }
-  //       else if ((epTranscript.epScenes.slice(-1)[0].sceneChars)) {
-  const $ = cheerio.load(node);
-  const sceneDialog = $("*").text();
-  const nonUniqueCharsInScene = sceneDialog.match(/[A-Z][?'A-Z]+(?=:)/g);
-  const dedupe = new Set(nonUniqueCharsInScene);
-  const uniqueCharsInScene = [...dedupe];
-  console.log("Chars in Scene:", uniqueCharsInScene.slice(0, 30));
-  return uniqueCharsInScene;
+const getChars = text => {
+  const nonUniqueChars = text.match(/[A-Z][^a-z\n]+(?=:)/g);
+  const nonUniqueCharsCleaned = nonUniqueChars.map(e =>
+    e.replace(/\[.*\]/, "").trim()
+  );
+  const deduped = new Set(nonUniqueCharsCleaned);
+  const uniqueChars = [...deduped];
+  return uniqueChars;
 };
 
 const getLinks = async url => {
