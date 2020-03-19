2a5b0b3f0f09767c10eac4270c4cc5461c1d5a85
eberling <c.eberling93@gmail.com>
2020-03-19 01:54:51 +0100

    adding concomitance and density fn

--- public/functions.js
+++ public/functions.js
@@ -2,6 +2,7 @@
 //    scene.chars.map(char =>)
 //  })
 
+//Chars can be specified , e.g. top 10 chars
 const calcEpisodeDensity = (chars, scenes) => {
   // episode density is average of all scene densities
   return (
@@ -10,11 +11,50 @@ const calcEpisodeDensity = (chars, scenes) => {
   );
 };
 
-const calcSceneDensity = (chars, scene) => {
-  return chars.length / scene.chars.length;
+const filterChars = (filter, chars) => {
+  return chars.filter(char => filter.includes(char));
 };
 
-const calcConcomitance = (chars, episode) => {};
+const filterCharsinScene = (filter, scene) => {
+  let sceneCpy = [...scene];
+  console.dir(sceneCpy, { depth: null });
+  sceneCpy.chars = filterChars(filter, sceneCpy.chars);
+  return sceneCpy;
+};
+
+const calcSceneDensity = (chars, scene) => {
+  const sceneChars = filterChars(chars, scene);
+  return sceneChars.length / chars.length;
+};
+
+const calcEpisodeConcomitance = (chars, scenes) => {
+  const scenesWithFilteredChars = scenes.map(scene =>
+    filterCharsinScene(chars, scene)
+  );
+  const charsWithSignature = chars.map(char => {
+    return {
+      char,
+      scenes: scenesWithFilteredChars.map(scene => {
+        scene.chars.includes(char) ? scene.title : null;
+      })
+    };
+  });
+  return (concomitantPairs = findIdenticalCharSignature(charsWithSignature));
+};
+
+const findIdenticalCharSignature = charsWithSignature => {
+  if (charsWithSignature.length < 2) {
+    return;
+  }
+  const firstChar = charsWithSignature[0];
+  const restChars = charsWithSignature.slice(1);
+  const concomitantPair = restChars.map(char => {
+    if (JSON.stringify(firstSig.scenes) == SON.stringify(char.scenes)) {
+      return firstSig;
+    }
+  });
+  return concomitantPair.concat(findIdenticalCharSignature(restChars));
+};
 
 const calcDominance = (chars, episode) => {};
 
